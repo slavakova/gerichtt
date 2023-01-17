1dea8d2e4cabefe994971e33650ece0e3a65fd09
slavakova <107358779+slavakova@users.noreply.github.com>
2023-01-17 13:58:07 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+        const menu = document.querySelector('.nav'),
+        menuItem = document.querySelectorAll('.nav__link'),
+        burger = document.querySelector('.burger'); 
+        
+        
+        burger.addEventListener('click', () => {
+            burger.classList.toggle('.burger__active');
+            
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+   
+
+	
+
+
+
